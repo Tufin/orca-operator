a26a9e905f8356ecf2512c00faae47735e1b47f4
shucks <reuvenharrison@gmail.com>
2019-04-09 12:35:11 +0300

    cluster-scope working

---
+++ pkg/apis/tufin/v1alpha1/orca_types.go
@@ -0,0 +1,56 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// OrcaSpec defines the desired state of Orca
+// +k8s:openapi-gen=true
+type OrcaSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	Domain             string            `json:"domain,omitempty"`
+	Project            string            `json:"project,omitempty"`
+	IngnoredConfigMaps []string          `json:"ignored_config_maps,omitempty"`
+	Components         map[string]bool   `json:"components,omitempty"`
+	EndPoints          map[string]string `json:"endpoints,omitempty"`
+	Images             map[string]string `json:"images,omitempty"`
+	KubePlatform       string            `json:"kube_platform,omitempty"`
+}
+
+// OrcaStatus defines the observed state of Orca
+// +k8s:openapi-gen=true
+type OrcaStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Orca is the Schema for the orcas API
+// +k8s:openapi-gen=true
+type Orca struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OrcaSpec   `json:"spec,omitempty"`
+	Status OrcaStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// OrcaList contains a list of Orca
+type OrcaList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Orca `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Orca{}, &OrcaList{})
+}
